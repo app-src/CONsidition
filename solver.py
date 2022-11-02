ab1fe44affe94894aa17955031afecbf5cba02cb
Magnus <magnus.sjostrand@consid.se>
2022-11-02 21:12:36 +0100

    Correct CO2 costs according to rules page

--- solver.py
+++ solver.py
@@ -5,8 +5,8 @@ from solution import Solution
 
 class Solver:
     bagType_price = [1.7, 1.75, 6, 25, 200]
-    bagType_co2_production = [5, 7, 3, 6, 20]
-    bagType_co2_transport = [50, 40, 60, 70, 100]
+    bagType_co2_transport = [3.9, 4.2, 1.8, 3.6, 12.0]
+    bagType_co2_production = [30, 24, 36, 42, 60]
 
     def __init__(self, game_info):
         self.days = None
