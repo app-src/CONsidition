b0b142195431f3a3245c8395d637ee7da007a99c
Ashish <38736403+ashishthehulk@users.noreply.github.com>
2022-11-07 19:42:09 +0530

    update in logic for result.txt

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ f = open("FancyvilleNEW.csv", 'w')
 writer = csv.writer(f)
 
 maxVals=[0 for i in range(31)]
+maxchoices=[]
 
 header=["score","bag_type","recycleRefundChoice","refundAmountMultiplicationFactor","choices1","choices2","choices3","choices4","choices5","choices6","choices7","choices8","choices9","choices10","choices11","choices12","choices13","choices14","choices15","choices16","choices17","choices18","choices19","choices20","choices21","choices22","choices23","choices24","choices25","choices26","choices27","choices28","choices29","choices30","choices31"]
 writer.writerow(header)
@@ -26,7 +27,7 @@ v = [0, 1, 2]
 
 def main():
 	# print("Starting game...")
-	global played_games, best_scores, best_scoresD, map_name,maxScore
+	global played_games, best_scores, best_scoresD, map_name,maxScore,maxchoices
 
 	for i in range(100):
 		# map_name = "Suburbia" if random.randint(0,2)==0 else "Fancyville"
@@ -62,12 +63,13 @@ def main():
 			played_games+=1
 			if score>maxScore:
 				maxScore=score
+				maxchoices=choices
 			
 			print("Game #{}: Score: {}".format(played_games, maxScore))
 			f2 = open("result.txt", 'w')
 			f2.write("Game #{}: Score: {}\n".format(played_games, maxScore))
 			# print(submit_game_response)
-			f2.write(str(choices))
+			f2.write(str(maxchoices))
 		
 			f2.close()
 
